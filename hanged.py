684d3aab94939d7232c95542dfa1fde442832278
Jordandiazfr <jordandiazfr@gmail.com>
2021-01-15 15:28:46 +0100

    Changed JSON for Random word API

--- hanged.py
+++ hanged.py
@@ -1,25 +1,18 @@
 from art import stages
-import json
-import random
+import requests
 
 
 def load_word():
-    f = open("words.json")
-    data = json.load(f)
-    return data['words']
-
-
-def select_winner():
-    choose = random.choice(load_word())
-    winner = [win.lower() for win in choose]
-    return winner
-
+    word = requests.get("https://random-word-api.herokuapp.com/word?number=1")
+    purified_word = word.text[2:-2]
+    list_word = [i for i in purified_word]
+    return list_word
 
 class Hanged:
     def __init__(self):
         self.life = 7
         self.life_art = stages
-        self.final_word = select_winner()
+        self.final_word = load_word()
         self.initial_blank = self.create_blanks()
         self.transition_word = self.initial_blank
         self.length = len(self.final_word)
--- main.py
+++ main.py
@@ -1,42 +1,38 @@
 from hanged import Hanged
 from art import logo
-game_on = True
-# Creating an instance
-hangman = Hanged()
-
-print(logo)
-#  Get the user guess input
-
-# Perpetual guess
-while game_on:
-    guess = input("Guess a letter: ").lower()
-    if guess in hangman.final_word:
-        hangman.transition(guess)
-    else:
-        hangman.life = hangman.life - 1
-        print("You  guessed: %s but is not in this word... you are getting hanged" % guess)
-        print(hangman.life_art[hangman.life])
-
-    print(hangman.transition_word)
-    # Stop the loop if user won
-    if hangman.transition_word == hangman.final_word:
-        print("Congratulations, You won ;) ")
-        game_on = False
-
-    if hangman.life == 0:
-        print("You loose, you got hanged! ")
-        print("Final word was: %s" % hangman.final_word
-              )
-        game_on = False
 
 
-
-
-
-''' 
 def main():
-    pass
+    game_on = True
+    # Creating an instance
+    hangman = Hanged()
+    print(hangman.final_word)
+    print(logo)
+    #  Get the user guess input
+
+    # Perpetual guess
+    while game_on:
+        guess = input("Guess a letter: ").lower()
+        if guess in hangman.final_word:
+            hangman.transition(guess)
+        else:
+            hangman.life = hangman.life - 1
+            print("You  guessed: %s but is not in this word... you are getting hanged" % guess)
+            print(hangman.life_art[hangman.life])
+
+        print(hangman.transition_word)
+        # Stop the loop if user won
+        if hangman.transition_word == hangman.final_word:
+            print("Congratulations, You won ;) ")
+            game_on = False
+
+        if hangman.life == 0:
+            print("You loose, you got hanged! ")
+            print("Final word was: %s" % hangman.final_word
+                  )
+            game_on = False
+
 
 if __name__ == '__main__':
     main()
-'''
+
