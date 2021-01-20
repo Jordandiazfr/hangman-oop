fc69ce12c802cf11e4a8496763ebc2598f5f50f0
Jordandiazfr <jordandiazfr@gmail.com>
2021-01-20 12:31:36 +0100

    logging updated 1.1

--- hanged.py
+++ hanged.py
@@ -2,16 +2,29 @@ from art import stages
 import requests
 import logging
 
+API = "https://random-word-api.herokuapp.com/word?number=1"
+
+def handle_input(input):
+    """ Checks the user input to make sure he puts only one letter from to A - Z """
+    if not input.isalpha() or len(input) > 1:
+        logging.debug("User input was not the expected")
+        print(" You must guess ONLY ONE letter from A to Z")
+        return False
+    return True
+
+
 def load_word():
+    """ Send a request to the selected api and is expecting a random string formmated ['string'] """
     try:
-        api = "https://random-word-api.herokuapp.om/word?number=1"
-        word = requests.get()
+        word = requests.get(API)
         purified_word = word.text[2:-2]
         list_word = [i for i in purified_word]
         logging.info("RANDOM WORD SUCCESFULLY LOADED FROM THE API")
+
         return list_word
     except:
-        logging.error("API ERROR: Couldn't complete the get request from: %s " % api)
+        logging.error("SERVER ERROR o USER IS NOT CONNECTED TO INTERNET: Couldn't complete the get request from: %s " % API)
+        print("THERE IS A SERVER ERROR, MAKE SURE YOU ARE CONNECTED TO THE INTERNET")
         return 0
 
 
@@ -24,6 +37,7 @@ class Hanged:
         self.transition_word = self.initial_blank
         self.length = len(self.final_word)
         self.guessed_words = []
+        logging.info("Object initiated")
 
     def create_blanks(self) -> list:
         blanks = ['_' for i in self.final_word]
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import logging
 import pathlib
 import os
-from hanged import Hanged
+from hanged import *
 from art import logo
+
 logfile = pathlib.Path(f'{os.getcwd()}/logs.log')
 logging.basicConfig(filename=logfile, format='%(asctime)s: %(levelname)s: %(message)s', level=logging.DEBUG,
-                         datefmt='[%Y-%m-%d %H:%M:%S]')
+                    datefmt='[%Y-%m-%d %H:%M:%S]')
 logging.info("Import of modules succeded")
+
 
 def main():
     game_on = True
@@ -16,28 +17,32 @@ def main():
     #  Get the user guess input
 
     # Perpetual guess
-    logging.info("Start of while loop")
+    logging.info("Start of game_on")
+
     while game_on:
+
         guess = input("Guess a letter: ").lower()
-        if guess in hangman.final_word:
-            hangman.transition(guess)
-        else:
-            hangman.life = hangman.life - 1
-            print("You  guessed: %s but is not in this word... you are getting hanged" % guess)
-            print(hangman.life_art[hangman.life])
+        if handle_input(guess):
+            if guess in hangman.final_word:
+                hangman.transition(guess)
+            else:
+                hangman.life = hangman.life - 1
+                print("You  guessed: %s but is not in this word... you are getting hanged" % guess)
+                print(hangman.life_art[hangman.life])
 
-        print(hangman.transition_word)
-        # Stop the loop if user won
-        if hangman.transition_word == hangman.final_word:
-            print("Congratulations, You won ;) ")
-            game_on = False
+            print(hangman.transition_word)
+            # Stop the loop if user won
+            if hangman.transition_word == hangman.final_word:
+                print("Congratulations, You won ;) ")
+                game_on = False
 
-        if hangman.life == 0:
-            print("You loose, you got hanged! ")
-            print("Final word was: %s" % hangman.final_word
-                  )
-            game_on = False
-    logging.info("End of while loop")
+            if hangman.life == 0:
+                print("You loose, you got hanged! ")
+                print("Final word was: %s" % hangman.final_word)
+                game_on = False
+
+    logging.info("End of game_on")
+
 
 if __name__ == '__main__':
     try:
@@ -47,4 +52,3 @@ if __name__ == '__main__':
         logging.exception("Unable to start the game")
         print("Unable to start the game, check the logs for more INFO about why")
         raise
-
